83d1674d06f5a32172f467ca5954321068ae2a7b
alvin2ye <alvin.ye.cn@gmail.com>
2013-08-08 14:47:59 +0800

    delete all

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-T1::Application.config.secret_token = 'b6fd067a23a795ac5d5301ddc0b599485e0da9c6d19f51fb82aca626a4e9bc49e0aee7cdc303bd7c22fafb9f5ba61d6ddbed6241f7c90ff8292f0fd6f881026d'
